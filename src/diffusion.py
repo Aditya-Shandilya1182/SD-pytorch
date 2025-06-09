24e0736640020a5b5d31476e5c09ce257498031b
Maverick18 <adityashandilyap22@gmail.com>
2025-06-09 11:14:59 +0530

    code added

---
+++ src/diffusion.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+from unet import UNET, TimeEmbedding, UNET_OutputLayer
+
+class Diffusion(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.time_embedding = TimeEmbedding(320)
+        self.unet = UNET()
+        self.final = UNET_OutputLayer(320, 4)
+    
+    def forward(self, latent, context, time):
+        time = self.time_embedding(time)
+        output = self.unet(latent, context, time)
+        output = self.final(output)
+        return output
